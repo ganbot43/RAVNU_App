5735816380729ce9d2de4982b41e058f854e9c30
AbigailMG90 <xcode@SUITECE106-ST02.local>
2026-04-21 18:20:28 -0500

    borre comentario

--- RAVNU_Proyecto/CuotasViewController.swift
+++ RAVNU_Proyecto/CuotasViewController.swift
@@ -21,7 +21,7 @@ final class CuotasViewController: UIViewController, UITableViewDataSource, UITab
     @IBOutlet private weak var lblClientesDeuda: UILabel?
     @IBOutlet private weak var emptyStateView: UIView?
 
-    private enum FiltroCuota { // hola 
+    private enum FiltroCuota {
         case todos
         case pendiente
         case vencido
